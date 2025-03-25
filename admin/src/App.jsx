88179b0069441734cde331dd9d65e0dd8be0a974
Mayank mehta <114612710+mayankmehta19032003@users.noreply.github.com>
2025-03-25 17:18:45 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer} from 'react-toastify';
 
 const App = () => {
   
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-zgss.onrender.com";
 
   return (
     <div>
